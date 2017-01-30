d666554840f7ad6cbf0fcb32feeba27963006aad
sohail0992 <sohail9099@yahoo.com>
2017-01-30 10:59:30 +0500

    basketball3d game github initial

---
+++ 3dBasketBall/Assets/scripts/UI_Controll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_Controll : MonoBehaviour 
+{
+	
+	public Text MyScore;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		MyScore.text = 	score.Gamescore.ToString ();
+
+	}
+}
---
+++ 3dBasketBall/Assets/scripts/score.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+    public GameObject Ball;
+	public static int Gamescore = 0;
+	public score()
+	{
+	}
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+  
+    private void OnTriggerEnter(Collider collision)
+    {
+        //Ball.gameObject.GetComponent<SphereCollider>().enabled =false;
+        Gamescore = Gamescore + 1;
+		Debug.Log("Score is : "+Gamescore);
+       
+    }
+
+
+
+
+} 
+
